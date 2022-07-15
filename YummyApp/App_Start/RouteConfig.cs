34c9ce9509e4303fb67bce1f881c3ed4166e8729
Ana Marija <anamarijaa@iborn.net>
2022-07-16 01:45:25 +0200

    Recipe Controller configuration

--- YummyApp/App_Start/RouteConfig.cs
+++ YummyApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace YummyApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Recipe", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- YummyApp/Controllers/RecipeController.cs
+++ YummyApp/Controllers/RecipeController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YummyApp.Models;
 
 namespace YummyApp.Controllers
 {
     public class RecipeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Recipe
         public ActionResult Index()
         {
-            return View();
+            var recipes = db.Recipes.ToList();
+            return View(recipes);
         }
     }
 }
