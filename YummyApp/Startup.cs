1e554657c27e4f273a3d745b0ec5d19fc248acdf
Ana Marija <anamarijaa@iborn.net>
2022-07-15 21:53:06 +0200

    Add project files.

---
+++ YummyApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YummyApp.Startup))]
+namespace YummyApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
